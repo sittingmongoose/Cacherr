9c8d8c89ff55f5073585a876ef0cecf7af01d222
sittingmongoose <sittingmongoose@gmail.com>
2025-09-06 21:19:39 -0700

    fix websocket_manager.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,2 @@
-# Cacherr - Docker-optimized Plex media caching system
-__version__ = "2.0.0"
+"""Top-level package for Cacherr Python modules."""
 
-# Make core modules accessible at package level
-from .core.plex_cache_engine import CacherrEngine
-from .config.settings import Config
-
-__all__ = ['CacherrEngine', 'Config']
